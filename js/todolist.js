52c2782a475eea2e7c23f35c9c3ec196382cae84
Anise-002 <n-u-002@naver.com>
2022-05-23 18:12:21 +0900

    todolist.js 완료

--- js/todolist.js
+++ js/todolist.js
@@ -4,30 +4,58 @@
     const todoInput = todoForm.querySelector("input");
     const todoList = todolistSection.querySelector("ul");
 
-    let todoListSave = [];
+    let todoListArr = [];
+
+    const TODO_KEY = "todo";
+
+    function saveToDo(todo){
+        localStorage.setItem(TODO_KEY, JSON.stringify(todo));
+    } 
 
     function deleteHnadler(e){
         const li = e.target.parentElement.parentElement;
+        const newArr = todoListArr.filter((item) => item.id !== parseInt(li.id));
+        todoListArr = newArr;
+        saveToDo(todoListArr);
         li.remove();
     }
 
-    function submitHandler(e){
-        e.preventDefault();
-        const todoInputValue = todoInput.value;
-        todoInput.value = null;
+    function showHtml(todo){
         //li 생성
         const li = document.createElement('li');
-        li.innerText = todoInputValue;
-        todoListSave.push(todoInputValue);
+        li.id = todo.id;
+        li.innerText = todo.text;
         const span = document.createElement('span');
         span.innerHTML = `<i class="fas fa-minus-circle"></i>`;
-        todoList.appendChild(li);
         li.appendChild(span);
+        todoList.appendChild(li);
 
         //list 삭제
         span.addEventListener('click',deleteHnadler);
     }
 
+    function submitHandler(e){
+        e.preventDefault();
+        const todoInputValue = todoInput.value;
+        const todoObject = {
+            text : todoInputValue,
+            id : Date.now()
+        }
+        todoInput.value = null;
+        showHtml(todoObject);
+        todoListArr.push(todoObject);
+        saveToDo(todoListArr);
+    }
 
     todoForm.addEventListener('submit',submitHandler);
+
+//로컬저장소에 데이터가 있는 경우 실행 함수
+    const savedToDo = localStorage.getItem(TODO_KEY);
+
+    if(savedToDo !== null){
+        parseToDo = JSON.parse(savedToDo); //문자 -> 배열
+        todoListArr = parseToDo;
+        todoListArr.forEach(showHtml);
+    }
+    
 })();
